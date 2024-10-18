da174120bea84adfd93e3921666624b196ca8862
reDragonCoder <redragoncybersec@gmail.com>
2024-10-17 20:40:48 -0600

    New C# file

---
+++ 3 Semester/Programming II/Codes/C#/firstCode/Program.cs
@@ -0,0 +1,8 @@
+﻿// AUthor: reDragonCoder
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My first C# code in VSC");
+
+string name="Cesar";
+int programmingLanguages=3;
+Console.WriteLine("Hi, I'm: "+name+" and I know "+programmingLanguages+" programming languages");
