4857c47ffd2d9b478a5825d29d6104816285f2f2
CesarMalanco <redragoncybersec@gmail.com>
2025-02-14 20:07:59 -0600

    New code

---
+++ 4 Semester/Differential Equations/Codes/campo_direccional.m
@@ -0,0 +1,27 @@
+% Script en Octave para graficar un campo direccional de y' = f(x,y)
+
+% Definir la función f(x, y) (se puede cambiar según el problema)
+f=@(x, y)  - x ./ y;
+;
+% Pedir al usuario los valores mínimo y máximo de la malla
+x_min = input('Ingrese el valor mínimo de x: ');
+x_max = input('Ingrese el valor máximo de x: ');
+y_min = input('Ingrese el valor mínimo de y: ');
+y_max = input('Ingrese el valor máximo de y: ');
+
+% Definir la malla de puntos
+delta = 0.5; % Tamaño de paso entre puntos en la malla
+[x, y] = meshgrid(x_min:delta:x_max, y_min:delta:y_max);
+
+% Calcular las direcciones del campo direccional
+u = ones(size(x));
+v = f(x, y);
+norm_factor = sqrt(1 + v.^2);
+u = u ./ norm_factor;
+v = v ./ norm_factor;
+
+% Graficar el campo direccional
+quiver(x, y, u, v, 'b');
+xlabel('x'); ylabel('y');
+title('Campo Direccional de y'' = f(x,y)');
+grid on;
