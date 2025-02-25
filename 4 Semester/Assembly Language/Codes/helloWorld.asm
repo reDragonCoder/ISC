6730dd0fef440afc91c27de26557ee2abc26eea2
CesarMalanco <redragoncybersec@gmail.com>
2025-02-25 08:09:23 -0600

    New code

---
+++ 4 Semester/Assembly Language/Codes/helloWorld.asm
@@ -0,0 +1,23 @@
+; Hello World en ensamblador
+; Celia Fernanda Vela Uribe - Cesar Andres Zuleta Malanco
+; 24/02/2025   
+; Ensamblador
+
+.model small    ; Se usa el modelo de memoria "small <Codigo y Datos = 64 kb>"
+.stack          ; Segmento de pila (stack), no se especifica tamano
+.data
+    msg db "Hello World, this is my first assembly program", "$"
+.code           ; Inicio del segmento de codigo
+
+main PROC           ; Inicio del procedimiento principal
+    MOV AX, SEG msg ; Obtiene el segmento donde esta almacenado msg y lo guarda en AX
+    MOV DS, AX      ; Mueve el valor de AX al registro DS (Data Segment)                                                               
+                    ; Ahora DS apunta al segmento donde esta msg  
+                    
+    MOV AH, 09H     ; Cargar la funcion 09H en AH (Imprimir cadena en DOS)
+    LEA DX, msg     ; Carga la direccion de msg en DX
+    INT 21H         ; Llama a la interrupcion 21H para imprimir la cadena en pantalla
+    
+    .exit           ; Indica el final del program
+main ENDP           ; Fin del procedimiento "main"
+END                 ; Fin del codigo fuente
---
+++ 4 Semester/Assembly Language/Codes/stringToHex.cpp
@@ -0,0 +1,47 @@
+// String to Hexadecimal
+// Celia Fernanda Vela Uribe - Cesar Andres Zuleta Malanco
+// 24/02/2025   
+// Ensamblador
+
+#include <iostream>
+
+using namespace std;
+string hexa(int n) {
+    string hexadecimal;
+    int aux;
+    while (aux>0) {
+        aux = n%16;
+        n = n/16;
+        if (aux >=10) {
+            hexadecimal += aux + '7';
+        } else {
+            hexadecimal += aux + '0';
+        }
+
+    }
+    return hexadecimal;
+}
+
+string flipNUmbers(string number){
+    string aux="";
+    for(int i=number.length(); i>=0; i--){
+        aux+=number[i];
+    }
+    return aux;
+}
+
+int main() {
+    string phrase = "Hello World, this is my first assembly program";
+    int aux;
+    string hexadecimalNum, aux2;
+    for(int i=0;i<phrase.length();i++) {
+        if (isspace(phrase[i])) {
+            cout << "020"<<"  ";
+        } else {
+            aux = phrase[i];
+            hexadecimalNum = hexa(aux);
+            aux2 = flipNUmbers(hexadecimalNum);
+            cout << aux2<<" ";
+        }
+    }
+}
