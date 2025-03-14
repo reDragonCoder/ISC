71d0a6954c36120b2c330b5dd91dfde56d7148af
CesarMalanco <redragoncybersec@gmail.com>
2025-03-14 15:14:03 -0600

    New code

---
+++ 4 Semester/Assembly Language/Codes/Vela_Zuleta_CodeC.cpp
@@ -0,0 +1,90 @@
+/* Exam code
+ Celia Fernanda Vela Uribe y Cesar Andres Zuleta Malanco
+ 10/03/2025
+ Assembly Language
+*/
+
+# include <iostream>
+
+using namespace std;
+
+string flipNUmbers(string number){
+    string aux="";
+    for(int i=number.length(); i>=0; i--){
+        aux+=number[i];
+    }
+    return aux;
+}
+
+string binaryConversion(int decimalNum){
+    string binaryNum, aux;
+    int cont=0;
+
+    while (decimalNum>0){
+        if (decimalNum%2!=0){
+            cont++;
+            binaryNum += "1";
+            decimalNum/=2;
+        } else{
+        binaryNum += "0";
+            decimalNum/=2;
+        }
+    }
+    aux = flipNUmbers(binaryNum);
+    return aux;
+}
+
+string baseConversion(int base, int decimal){
+    // The converted number will be handled as a string to make things easier when adding numbers and flipping it. 
+    string num, finalNum;
+    int cont=0;
+
+    while (decimal>0){
+        // If the number is evenly divisible by the base we automatically add a 0
+        if (decimal%base==0){
+            num +="0";
+            decimal/=base;
+        // Otherwise we first check that it is bigger than the base and thus divisible without causing a zero
+        } else if(decimal>=base){
+            cont = decimal % base;
+            num += cont + '0';
+            decimal/=base;
+        // If the number is indeed lesser than the base we automatically add it to the string
+        }else if(decimal<base){
+            num += decimal + '0';
+            decimal = 0;
+        }
+    }
+    // The answer must be flipped before returning it as the converted number. 
+    finalNum = flipNUmbers(num);
+    return finalNum;
+}
+
+int main(){
+    int n=0, t=0, decimal=0;
+    // The user is first asked how many times he would like to convert a number or basically try the program
+    string conversedNum;
+    cout<<"How many times would you like to convert a number?: ";
+    cin>>t;
+    while(t--){
+        // As a safety percaution we clear the variablse that will be used through the cycle in order to better guarantee the smooth sailing of the program
+        n=0;
+        decimal = 0;
+        conversedNum = " ";
+        cout<<"Please enter the base to which you would like to convert the decimal number?: ";
+        cin>>n;
+        cout<<"Please enter the number that you would like to convert!: ";
+        cin>>decimal;
+        if (n == 2){
+            conversedNum = binaryConversion(decimal);
+        } else {
+            conversedNum = baseConversion(n,decimal);
+        }
+        // The number is printed
+        cout<<"The number: "<<decimal<<" when converted to base "<<n<<" is: "<<conversedNum<<endl;
+        cout<<endl;
+    }
+
+    //
+    return 0;
+}
---
+++ 4 Semester/Assembly Language/Codes/examCode.asm
@@ -0,0 +1,81 @@
+; Exam code
+; Celia Fernanda Vela Uribe y Cesar Andres Zuleta Malanco
+; 10/03/2025
+; Assembly Language
+
+.model
+.stack
+.data
+    name1 db "Cesar Zuleta", "$" 
+    char db " ", "$"
+    endl db 10, 13, "$"
+    name2 db "Fernanda Vela", "$"
+.code
+
+main PROC   
+    ; PRINT NAMES
+    MOV AX, SEG name1
+    MOV DS, AX
+    
+    MOV BX, SEG name2
+    MOV DS, BX   
+    
+    MOV AH, 09H
+    LEA DX, name1
+    INT 21H  
+    
+    MOV AH, 09H
+    LEA DX, endl
+    INT 21H
+    
+    MOV BH, 09H
+    LEA DX, name2
+    INT 21H
+    
+    ; INITIALIZATION
+    MOV AX, @data
+    MOV DS, AX
+    
+    ; ADD
+    MOV AL, 74h
+    ADD AL, 65h
+    
+    ; SUB
+    MOV AL, 74h
+    SUB AL, 65h
+    
+    ; MUL BYTE
+    MOV AL, 74h
+    MOV BL, 65h
+    MUL BL
+    
+    ; MUL WORD - RESULT * 500
+    MOV BX, 1F4h
+    MUL BX
+    
+    ; DIV WORD/WORD
+    MOV AX, 74h
+    MOV BL, 65h
+    DIV BL
+    
+    ; DIV DWORD/WORD
+    MOV DX, 20h
+    MOV AX, 65h    
+    MOV BX, 74h
+    DIV BX 
+    
+    MOV AL, 74h
+    ;MOV BL, 65h
+    CMP AL, 65h
+    JNZ RESULT_IS_NOT_ZERO
+    
+    MOV AL, 1  
+    
+    RESULT_IS_NOT_ZERO: 
+        MOV AL, 50
+    
+    
+    .exit
+
+main ENDP
+END main
