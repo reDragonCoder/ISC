8657c82441dc5e4f6d50486913bd066549ec3f84
CesarMalanco <redragoncybersec@gmail.com>
2025-03-18 19:51:08 -0600

    New code

---
+++ 4 Semester/Assembly Language/Codes/Unit_2/cicloCreciente.asm
@@ -0,0 +1,42 @@
+; Ciclo creciente
+; Celia Fernanda Vela Uribe y Cesar Andres Zuleta Malanco
+; 18/03/2025
+; Assembly Language  
+
+.model small
+
+.stack
+
+.data
+    arreglo_1 DB 1,2,3,4,5,6,7,8,9,10
+    arreglo_2 DB 10 dup(0)
+    
+.code
+main PROC
+    MOV AX, @data   ; Inicializacion del segmento de datos
+    MOV DS, AX
+    
+    ; SI: Source Index
+    MOV si, 0   
+    ; DI: Destination Index
+    MOV DI, 0
+    
+    BUCLE:
+    MOV AL, arreglo_1[SI]
+    MOV arreglo_2[DI], AL
+    INC SI  ; Incrementar SI
+    INC DI  ; Incrementar DI
+    CMP DI, 10
+    JL BUCLE    ; JL: Jump if Less
+    JMP TERMINAR
+    
+    TERMINAR:
+    ; Terminar programa correctamente
+    MOV AH, 4Ch ; Funcion 4Ch: Terminar programa en DOS
+    INT 21h ; Llamada a interrupcion DOS
+
+main ENDP
+END main
+
+
+
---
+++ 4 Semester/Assembly Language/Codes/Unit_2/cicloDecreciente.asm
@@ -0,0 +1,39 @@
+; Ciclo decreciente
+; Celia Fernanda Vela Uribe y Cesar Andres Zuleta Malanco
+; 18/03/2025
+; Assembly Language  
+
+.model small
+
+.stack
+
+.data
+    arreglo_1 DB 1,2,3,4,5,6,7,8,9,10
+    arreglo_2 DB 10 dup(0)
+    
+.code
+main PROC
+    MOV AX, @data   ; Inicializacion del segmento de datos
+    MOV DS, AX
+    
+    ; SI: Source Index
+    MOV si, 9   ; Ultima posicion del arreglo, lo usaremos para el ciclo
+    ; DI: Destination Index
+    MOV DI, 0
+    
+    BUCLE:
+    MOV AL, arreglo_1[SI]
+    MOV arreglo_2[DI], AL
+    DEC SI  ; Decrementar SI
+    INC DI  ; Incrementar DI
+    CMP DI, 10
+    JL BUCLE    ; JL: Jump if Less
+    JMP TERMINAR
+    
+    TERMINAR:
+    ; Terminar programa correctamente
+    MOV AH, 4Ch ; Funcion 4Ch: Terminar programa en DOS
+    INT 21h ; Llamada a interrupcion DOS
+
+main ENDP
+END main
